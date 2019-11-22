619bd45fc8de95f572a96dc9aed71abf97544fcb
luoqian <szuluoqian@163.com>
2019-11-22 17:09:19 +0800

    feat: add yuumi unit-test

--- __test__/redux/index.test.ts
+++ __test__/redux/index.test.ts
@@ -1,9 +1,9 @@
 import { expectSaga } from 'redux-saga-test-plan';
-import { watchFetchDateList, watchFetchTodoList } from './saga';
+import { watchFetchDateList, watchFetchTodoList } from 'example/redux/index/saga';
 import { fetchDateList, fetchTodoList } from 'example/api/fake-api';
 import { call, select } from 'redux-saga/effects';
 import { ReduxState } from 'example/redux/root-reducer';
-import { indexModel, update_select_date, update_todo_list, TodoDataInfo, selectSelectedDate, defaultState, fetch_date_list, update_date_list } from './index';
+import { indexModel, update_select_date, update_todo_list, TodoDataInfo, selectSelectedDate, defaultState, fetch_date_list, update_date_list } from 'example/redux/index/index';
 
 describe('sagas test', () => {
   test('watchFetchDateList saga run right', () => {
@@ -23,6 +23,7 @@ describe('sagas test', () => {
   test('watchFetchTodoList run right', () => {
     const store:ReduxState = {
       indexState: { ...defaultState, select_date: '2019-11-11' },
+      LoginSate: { test: '' },
     };
     const fakeTodoList:TodoDataInfo[] = [{ id: 'jjj', date: '2019-11-11', isFinished: false, overview: 'new day', details: 'hello' }];
     return expectSaga(watchFetchTodoList)
--- jest.config.js
+++ jest.config.js
@@ -1,29 +1,4 @@
-const jsdom = require('jsdom');
-
 module.exports = {
-  testEnvironment: 'jsdom',
-  transform: {
-      ".(ts|tsx)": "ts-jest"
-  },
-  moduleFileExtensions: [
-    'ts',
-    'tsx',
-    'js',
-    'jsx',
-  ],
-  transformIgnorePatterns: [
-    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$',
-    '^.+\\.module\\.(css|sass|scss|less)$',
-    '/cypress/',
-  ],
-  moduleNameMapper: {
-    '^example/(.*)$': '<rootDir>/src/$1'
-  },
-  moduleDirectories: ["node_modules", "src"], 
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
-  coverageDirectory: 'coverage',
-  collectCoverageFrom: [
-    'src/**/*.{ts,tsx,js,jsx}',
-    '!src/**/*.d.ts',
-  ],
-};
+  preset: '@mlz/cli-plugin-unit-jest', 
+  testRegex: '(/__tests__/.*|(\\.|/)(test))\\.(ts|js)x?$',
+}
